c3fb16dbd1441872fb82f79b0794a499fc5055fa
Dudley Esteban Díaz Suárez <deds15@gmail.com>
2018-09-07 16:11:52 -0500

    Se añaden funciones de extracción

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="AutoML",
+    version="0.1.0",
+            url="https://github.com/resuelve/lead-score/utils",
+    description="Utilities for data science teams.",
+    long_description=open('README.md').read(),
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
